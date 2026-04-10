8f3e6d6f0b437cb5d2a68eaeb886d53ebbcab933
JohnAppleseedTM <global.appleseed@gmail.com>
2026-04-10 19:36:42 +0300

    Allowing host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  preview: {
+    allowedHosts: ["frontend-production-3f56.up.railway.app"],
+  },
 });
