b9e829af8583cb262c5ec581b378ccb4fc6b86a9
JohnAppleseedTM <global.appleseed@gmail.com>
2026-04-10 19:52:43 +0300

    Added download/db path for file downloading

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 from backend.database import get_connection, create_table_temp, create_table_air
 from backend.fetcher import run
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 import threading
+import os
 
 app = FastAPI()
 
@@ -24,6 +26,15 @@ def startup():
     create_table_air()
     thread = threading.Thread(target=run, daemon=True)
     thread.start()
+
+@app.get("/download/db")
+def download_db():
+    return FileResponse(
+        path=os.path.join(os.path.dirname(__file__), "data/pythonlite.db"),
+        filename="pythonlite.db",
+        media_type="application/octet-stream"
+    )
+
 
 @app.get("/temp")
 def fetch_temp():
